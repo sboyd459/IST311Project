b2330210c6e5eeddaf1dce77bacdd00f682338b2
Sydney Boyd <sydneybsb21@gmail.com>
2020-04-22 23:29:07 -0400

    Model code

---
+++ src/Model/Company.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Sydney
+ */
+@Entity
+@Table(name = "Company")
+@NamedQueries({
+    @NamedQuery(name = "Company.findAll", query = "SELECT c FROM Company c"),
+    @NamedQuery(name = "Company.findByCompanyID", query = "SELECT c FROM Company c WHERE c.companyID = :companyID"),
+    @NamedQuery(name = "Company.findByCompanyName", query = "SELECT c FROM Company c WHERE c.companyName = :companyName"),
+    @NamedQuery(name = "Company.findByCompanyDescription", query = "SELECT c FROM Company c WHERE c.companyDescription = :companyDescription")})
+public class Company implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Basic(optional = false)
+    @Column(name = "CompanyID")
+    private int companyID;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "CompanyName")
+    private String companyName;
+    @Basic(optional = false)
+    @Column(name = "CompanyDescription")
+    private String companyDescription;
+
+    public Company() {
+    }
+
+    public Company(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public Company(String companyName, int companyID, String companyDescription) {
+        this.companyName = companyName;
+        this.companyID = companyID;
+        this.companyDescription = companyDescription;
+    }
+
+    public int getCompanyID() {
+        return companyID;
+    }
+
+    public void setCompanyID(int companyID) {
+        this.companyID = companyID;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCompanyDescription() {
+        return companyDescription;
+    }
+
+    public void setCompanyDescription(String companyDescription) {
+        this.companyDescription = companyDescription;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (companyName != null ? companyName.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Company)) {
+            return false;
+        }
+        Company other = (Company) object;
+        if ((this.companyName == null && other.companyName != null) || (this.companyName != null && !this.companyName.equals(other.companyName))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Model.Company[ companyName=" + companyName + " ]";
+    }
+    
+}
